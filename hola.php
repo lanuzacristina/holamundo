826e7030ab5f4174b472a0828fb946576915957f
lanuzacristina <lanuzacristina@gmail.com>
2019-06-17 22:00:31 +0200

    He afegit la edat.

--- hola.php
+++ hola.php
@@ -1,5 +1,6 @@
 <?php
 $nom = "Cristina";
 $llinage = "Lanuza";
+$edat = "23";
 $nom_complet = $nom." ".$llinage;
-echo("Hola, soc $nom_complet.");
+echo("Hola, soc $nom_complet i tinc $edat anys.");
