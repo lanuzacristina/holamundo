1860898f233ee77a5ae15b887bdd95c9e2d9828e
lanuzacristina <lanuzacristina@gmail.com>
2019-06-18 18:43:55 +0200

    En la edat mire si me venen alcohol, si es una edat posible i si me falta poc per a morir en operadors condicionals i comparadors.

--- hola.php
+++ hola.php
@@ -1,6 +1,23 @@
 <?php
 $nom = "Cristina";
 $llinage = "Lanuza";
-$edat = "23";
+$edat = 23;
 $nom_complet = $nom." ".$llinage;
+
 echo("Hola, soc $nom_complet i tinc $edat anys.");
+
+if ($edat >= 18) [
+    echo("¡Puc comprar alcohol en la discoteca!");
+] else [
+    echo("Si dema alcohol en la discoteca no me la donen i me muic de ganes i de vergonyeta.");
+]
+if ($edat == true) [
+    echo("Pot ser que tinga eixa edat, sí.");
+] else [
+    echo("Eixa edat no existix, primo.");
+]
+if ($edat >= 90) [
+    echo("Va quedant-me poquet per a la mort.");
+] else [
+    echo("¡Olé el bro lo joveneta que soc!");
+]
