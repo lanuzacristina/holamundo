70e74f745efafb85303b9a6092dbf5aa9a1f0ec8
lanuzacristina <lanuzacristina@gmail.com>
2019-06-19 20:01:11 +0200

    Millora - controlar que la edat pot ser correcta.

--- hola.php
+++ hola.php
@@ -4,23 +4,18 @@ $llinage = "Lanuza";
 $edat = 23;
 $nom_complet = $nom." ".$llinage;
 
-echo("Hola, soc $nom_complet i tinc $edat anys.");
-echo("<br>");
-if ($edat >= 18) {
-    echo("¡Puc comprar alcohol en la discoteca!");
+echo("Hola, soc $nom_complet i tinc $edat anys.<br>");
+
+if ($edat ==false || is_float($edat) || $edat > 105 || $edat < 0) {
+    echo("¡Fica be l'edat, che!<br>");
+} else if ($edat >= 18) {
+    echo("¡Puc comprar alcohol en la discoteca!<br>");
 } else {
-    echo("Si dema alcohol en la discoteca no me la donen i me muic de ganes i de vergonyeta.");
+    echo("Si dema alcohol en la discoteca no me la donen i me muic de ganes i de vergonyeta.<br>");
 }
-echo("<br>");
-if ($edat == true) {
-    echo("Pot ser que tinga eixa edat, sí.");
+if ($edat ==false || is_float($edat) || $edat > 105 || $edat < 0) {
+} else if ($edat >= 90) {
+    echo("Va quedant-me poquet per a la mort.<br>");
 } else {
-    echo("Eixa edat no existix, primo.");
+    echo("¡Olé el bro lo joveneta que soc!<br>");
 }
-echo("<br>");
-if ($edat >= 90) {
-    echo("Va quedant-me poquet per a la mort.");
-} else {
-    echo("¡Olé el bro lo joveneta que soc!");
-}
-    
