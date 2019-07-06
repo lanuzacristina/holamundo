8048afcae624949d952bb2a36e30116108d1543b
lanuzacristina <lanuzacristina@gmail.com>
2019-07-06 13:19:04 +0200

    Que te demane la edat.

--- hola.php
+++ hola.php
@@ -1,26 +1,27 @@
 <?php
 $nom = "Cristina";
 $llinage = "Lanuza";
-$edat = 23;
 $nom_complet = $nom." ".$llinage;
+$saltodelinea = PHP_EOL;
+$edat = readline("Introduix la teua edat: ");
 
-echo("Hola, soc $nom_complet i tinc $edat anys.<br>");
+echo("Hola, soy $nom_complet i tinc $edat anys.$saltodelinea");
 
 if ($edat ==false || is_float($edat) || $edat > 105 || $edat < 0) {
     
-    echo("¡Fica be l'edat, che!<br>");
+    echo("¡Fica be l'edat, che!$saltodelinea");
 
 } else {    
     
     if ($edat >= 18) {
-        echo("¡Puc comprar alcohol en la discoteca!<br>");    
+        echo("¡Puc comprar alcohol en la discoteca!$saltodelinea");    
     } else {
-        echo("Si demane alcohol en la discoteca no me la donen i me muic de ganes i de vergonyeta.<br>");
+        echo("Si demane alcohol en la discoteca no me la donen i me muic de ganes i de vergonyeta.$saltodelinea");
     }
     
    if ($edat >= 90) {
-        echo("Va quedant-me poquet per a la mort.<br>");
+        echo("Va quedant-me poquet per a la mort.$saltodelinea");
     } else {
-        echo("¡Olé el bro lo joveneta que soc!<br>");
+        echo("¡Olé el bro! ¡Què joveneta que soc!$saltodelinea");
     } 
 }
